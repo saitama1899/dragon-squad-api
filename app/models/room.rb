86e188a53c790f831476bca59cab3a26cceab3f3
Bertys <bertysitu@gmail.com>
2020-01-23 13:29:22 +0100

    Revert "First version of Room Model and validations test."
    This reverts commit 2e71eed3ae65facf919b717f302f87057794e208.

--- app/models/room.rb
+++
@@ -1,7 +0,0 @@
-class Room < ApplicationRecord
-    validates_presence_of :title
-    validates_presence_of :description
-    validates_presence_of :price
-    validates_presence_of :lat
-    validates_presence_of :lng
-end
--- spec/models/room_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Room, type: :model do
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:description) }
-  it { should validate_presence_of(:price) }
-  it { should validate_presence_of(:lat) }
-  it { should validate_presence_of(:lng) }
-end
