657bb43ff094da51531d1847622d761333199025
Bertys <bertysitu@gmail.com>
2020-01-24 12:45:46 +0100

    Adding photo Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_23_192855) do
+ActiveRecord::Schema.define(version: 2020_01_24_111943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.string "url"
+    t.bigint "room_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_photos_on_room_id"
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string "title"
@@ -26,4 +34,5 @@ ActiveRecord::Schema.define(version: 2020_01_23_192855) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "photos", "rooms"
 end
