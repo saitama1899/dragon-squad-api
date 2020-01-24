2dfe4e68834ec5f080c1560d21e60e602944a57e
Eric Selva <eric.lorien92@gmail.com>
2020-01-24 17:36:58 +0100

    First error handle working for tests

--- app/api/badi/v1/helpers/exception_handler.rb
+++ app/api/badi/v1/helpers/exception_handler.rb
@@ -1,12 +1,8 @@
-module Badi
-  class Api < Grape::API
+module Helpers
+  module ExceptionHandler
+    # extend Grape::API::Helpers
+    extend ActiveSupport::Concern
 
-
-    format :json
-    content_type :json, 'application/json; charset=utf-8'
-
-    # I will try to include this soon!
-    # include Helpers
     class Grape::Middleware::Error
       def error_message(code, text)
         {
@@ -21,9 +17,5 @@ module Badi
     rescue_from ActiveRecord::RecordNotFound do |e|
       rack_response(error_message(:not_found, 'Record was not found'), 404)
     end
-
-    mount Badi::Health
-    mount Badi::V1::Rooms
-    mount Badi::V1::Locations
   end
 end
