c955f1b63f7ba37300d6fe2f2ee1ec7163d7aa49
Eric Selva <eric.lorien92@gmail.com>
2020-01-24 22:51:04 +0100

    Revert "Photo entity"

--- app/api/badi/entities/photo.rb
+++
@@ -1,7 +0,0 @@
-module Badi
-  module Entities
-    class Photo < Grape::Entity
-      expose :url
-    end
-  end
-end
--- db/migrate/20200123192855_change_description_to_be_text_in_rooms.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDescriptionToBeTextInRooms < ActiveRecord::Migration[6.0]
-  def change
-    change_column :rooms, :description, :text
-  end
-end
