d5ad1c6c5989870d1e44e445efd26aedff139248
saitama1899 <eric.lorien92@gmail.com>
2020-01-25 19:33:36 +0100

    Refactored seeds and room model validations to fit Barcelona boundaries

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -5,6 +5,6 @@ RSpec.describe Room, type: :model do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
   it { should validate_presence_of(:price) }
-  it { should validate_presence_of(:lat) }
-  it { should validate_presence_of(:lng) }
+  it { should validate_presence_of(:lat).is_at_least(40).is_at_most(42) } # Barcelona
+  it { should validate_presence_of(:lng).is_at_least(1).is_at_most(3) }
 end
