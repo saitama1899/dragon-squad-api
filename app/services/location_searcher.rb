4056d84ffea6ae03408fc982bd7b1c59879e1397
saitama1899 <eric.lorien92@gmail.com>
2020-01-27 19:58:46 +0100

    Location searcher hoy fix

--- app/services/location_searcher.rb
+++ app/services/location_searcher.rb
@@ -4,11 +4,8 @@ require 'json'
 class LocationSearcher
   API_GEOCODE = OpenCage::Geocoder.new(api_key: ENV['API_KEY'])
 
-  CITY_KEY = "city"
-  ROAD_KEY = "road"
-
-  def self.call(location, country)
-    possible_locations = API_GEOCODE.geocode(location, country_code: country, language: 'es')
+  def self.call(location)
+    possible_locations = API_GEOCODE.geocode(location)
 
     results = []
     possible_locations.each do |item|
