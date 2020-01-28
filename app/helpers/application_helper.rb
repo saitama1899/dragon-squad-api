6ae1999ca5e44b9a85aff026b1958f55190d77c3
saitama1899 <eric.lorien92@gmail.com>
2020-01-28 20:59:02 +0100

    First suggest locations method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,21 +2,10 @@ module ApplicationHelper
   require 'faraday'
   require 'json'
 
-
-
   def suggest_locations(keyword)
     url = 'http://localhost:3000/api/v1/locations'
     request = Faraday.get(url, {location: keyword})
-    # url = "http://localhost:3000/api/v1/rooms?lat=40.43442881674111&lng=-3.548318834259536&range=5000 "
-    # request = Faraday.get url
-    # puts request.body
-    puts JSON.parse(request.body)['address']
-
-    # arr = []
-    # results.each do |child|
-    #   arr << child['components']
-    # end
-
-    # return arr
+    results = JSON.parse(request.body)
   end
+
 end
