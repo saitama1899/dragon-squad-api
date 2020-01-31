b75e39359fd3c8ed67c800a9f566fef868eb55c7
saitama1899 <eric.lorien92@gmail.com>
2020-01-31 13:01:38 +0100

    Added first opencage spec

---
+++ spec/requests/v1/external_requests_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+# rspec ./spec/requests/v1/external_requests_spec.rb
+
+feature 'OpenCage external request' do
+  it 'queries for a location' do
+    stub_request(:get, "https://api.opencagedata.com/geocode/v1/json?key=a07e1766190643b79d64cf4ea64ab4b3&q=mad").
+    with(
+      headers: {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'Host'=>'api.opencagedata.com',
+        'User-Agent'=>'Ruby'
+      }).
+    to_return(status: 200, body: "", headers: {})
+  end
+end
