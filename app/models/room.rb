ee434fa064c8e73ad1e0742bc2fb7617cd998a99
Vaniik <ivan.alvarez.enri.oficial@gmail.com>
2020-02-01 12:18:49 +0100

    model with after_create and before_delete methods

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,4 +4,28 @@ class Room < ApplicationRecord
   validates_presence_of :title, length: { in: 6..30 }
   validates_presence_of :description, length: { maximum: 300 }
   validates_presence_of :price, numericality: true, greater_than: 0
+  class Room < ApplicationRecord
+    has_many :photos, dependent: :destroy
+    belongs_to :location
+    validates_presence_of :title, length: { in: 6..30 }
+    validates_presence_of :description, length: { maximum: 300 }
+    validates_presence_of :price, numericality: true, greater_than: 0
+    after_create :roomIncrement
+
+    #callbacks
+    private
+      def roomIncrement
+        location=Location.find_by_id(self.location_id)
+        location.total_rooms += 1
+        location.save
+
+      end
+      def roomDecrement
+        location=Location.find_by_id(self.location_id)
+        location.total_rooms -= 1
+        location.save
+      end
+
+  end
+
 end
