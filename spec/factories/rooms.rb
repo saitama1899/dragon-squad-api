a8d9beda410ba3d3772720f0b8ad1d8901f751a8
Vaniik <ivan.alvarez.enri.oficial@gmail.com>
2020-02-04 21:59:49 +0100

    factory done for the new room model

--- spec/factories/rooms.rb
+++ spec/factories/rooms.rb
@@ -6,6 +6,13 @@ FactoryBot.define do
     description { Faker::Lorem.paragraph }
     price { Faker::Number.number(digits: 3) }
     owner { Faker::Games::Pokemon.name }
+    bills_included{Faker::Boolean.boolean}
+    deposit{Faker::Boolean.boolean}
+    verified{Faker::Boolean.boolean}
+    roommate_girls { Faker::Number.number(digits: 1) }
+    roommate_boys { Faker::Number.number(digits: 1) }
+    room_size { Faker::Number.number(digits: 2) }
+    property_size { Faker::Number.number(digits: 3) }
     location_id { nil }
   end
 end
