a2f691cb865be9ec42567ae73ed6985baaded690
saitama1899 <eric.lorien92@gmail.com>
2020-02-04 16:12:17 +0100

    comment about this unfinished branch

--- config/initializers/cron_jobs.rb
+++ config/initializers/cron_jobs.rb
@@ -9,3 +9,6 @@ motivation = BeADragon::MotivateMePls
 scheduler.cron '* * * * *' do
   motivation.random_sentence('Eric')
 end
+
+# In process to add cron task that puts on true if a room
+# is onfire (top more visited in last hour)
