5460cd530bde853a076f13a755841c9f7c36bd30
saitama1899 <eric.lorien92@gmail.com>
2020-02-04 10:58:48 +0100

    Fixed motivational gem

--- config/initializers/cron_jobs.rb
+++ config/initializers/cron_jobs.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 require 'rufus-scheduler'
+require 'be_a_dragon'
 
 scheduler = Rufus::Scheduler.new
+motivation = BeADragon::MotivateMePls
 
 scheduler.cron '* * * * *' do
-  BeADragon::MotivateMePls.random_sentence('Eric')
+  motivation.random_sentence('Eric')
 end
